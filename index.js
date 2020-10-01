d218797e3b04cece9c752629d31611ebe0f0ef96
Damian Barrios <barrios.d122@gmail.com>
2020-10-01 18:31:54 -0300

    added miniApi

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 
 //requiring routes
 const routes = require('./routes');
+const routesApi = require('./routes-api')
 
 //Settings
 app.set('appName', 'First Server');
@@ -15,20 +16,9 @@ app.set('view engine', 'ejs');
 app.use(morgan('short'));
 app.use(morgan('combined')); */
 
-
-//middlewares
-/* app.use(function(req,res, next) {
-    console.log('request url: ' + req.url);
-    next();
-});
-
-app.use( (req, res,next) => {
-    console.log("Function 2");
-    next();
-}); */
-
 //routes
 app.use(routes);
+app.use(routesApi);
 
 app.listen(3000, () => {
     console.log("Server on port 3000");
