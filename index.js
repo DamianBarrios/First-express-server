120152fe719277b96970846971c9fb10a0681733
Damian Barrios <barrios.d122@gmail.com>
2020-10-01 15:51:41 -0300

    routes added

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ app.get('/', (req, res) => {
     res.send("Hello World")
 });
 
+app.get('/login', (req, res ) => {
+    res.send("Login Page")
+})
+
+app.get('*', (req, res) => {
+    res.writeHead(404, {'Content-type': 'text/html'})
+    res.write('<h1>Not Found</h1>')
+    
+})
+
 app.listen(3000, () => {
     console.log("Server on port 3000")
 })
